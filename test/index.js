5d0d13375887cd375d6b7450e61ffdfc818cd457
andrewholsted <andrew@fajitanachos.com>
2015-11-26 07:06:03 -0700

    Add unit testing

---
+++ test/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Setup tests
+before(function(done) {
+	done();
+});
+
+// Teardown tests
+after(function(done) {
+	done();
+});
---
+++ test/middleware/socket/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let expect = require('chai').expect;
+let io = require('socket.io-client');
+
+describe('socket', function() {
+  it('should accept incoming connections', function () {
+  });
+});
