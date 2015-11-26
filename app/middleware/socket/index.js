aea8f802a7b0ae3efb96c4cdb0a2718e36f7f844
andrewholsted <andrew@fajitanachos.com>
2015-11-25 22:18:35 -0700

    Initial server setup

---
+++ app/middleware/socket/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+ 	console.log('connected');
+});
--- server.js
+++ server.js
@@ -1,18 +1,23 @@
-let cluster = require('cluster');
+'use strict';
+
 let numCPUs = require('os').cpus().length;
+let cluster = require('cluster');
 
 if (cluster.isMaster) {
-  // Fork workers.
+  // Fork workers up to availabled cpus
   for (let i = 0; i < numCPUs; i++) {
-    let worker = cluster.fork();
+    cluster.fork();
   }
 
+  // Exit event is fired whenever a worker process exits.
   cluster.on('exit', function(worker, code, signal) {
-    console.log('worker ' + worker.process.pid + ' died');
-  });
-
-
+    console.log('worker ' + worker.process.pid + ' exited.');
+    
+    // Restart the worker
+    let newWorker = cluster.fork();
+    console.log('worker ' + newWorker.process.pid + ' created.');
+  });	
 } else {
-  // Start app
-
+  // Start the app
+  require('./app');
 }
