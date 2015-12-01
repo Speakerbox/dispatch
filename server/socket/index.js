44bc1cfebe7af68e1c528ccee999270c7299a109
andrewholsted <andrew@fajitanachos.com>
2015-12-01 07:08:50 -0700

    Add a couple socket tests

--- server/socket/index.js
+++ server/socket/index.js
@@ -13,11 +13,28 @@ function init(){
 
   // Middleware for handling authentication
   io.use(function(socket, next){
-    next();
+    let token = socket.handshake.query.token;
+
+    if(!token){
+      let err = { message: 'Must provide a token' };
+      next(err);
+      return;
+    }
+
+    authenticate(token)
+      .then(function(){
+        next();
+      })
+      .catch(function(err){
+        next(err);
+      });
   })
 
   io.on('connect', function(socket) {
-  
+    console.log('connected');
   });
+}
+
+function authenticate(token){
 
 }
--- test/socket/index.js
+++ test/socket/index.js
@@ -7,11 +7,40 @@ let socket;
 
 describe('socket', function() {
 
-  it('should not allow unauthorized connections', function(done) {
+  it('should not connect without a token', function(done) {
   	let socketUrl = nconf.get('socket:url');
   	socket = ioClient.connect(socketUrl);
-  	socket.on('connect', function(){
-  		done();	
-  	})
+
+  	socket.on('error', function(err){
+  		expect(err).to.equal('Must provide a token');
+  		expect(socket.connected).to.equal(false);
+	  	socket.disconnect();
+	  	done();
+  	});
+  	
   });
+
+  it('should not connect with an invalid token', function(done) {
+  	let socketUrl = nconf.get('socket:url');
+  	socket = ioClient.connect(socketUrl + '?token=XYLznXKRDwMsshjRJFpBsLngUiEyAx');
+
+  	socket.on('error', function(err){
+  		expect(err).to.equal('Must provide a token');
+  		expect(socket.connected).to.equal(false);
+	  	socket.disconnect();
+	  	done();
+  	});
+  	
+  });
+
+  // it('should not connect without an invalid token', function(done) {
+  // 	let socketUrl = nconf.get('socket:url');
+  // 	socket = ioClient.connect(socketUrl + '?token=XYLznXKRDwMsshjRJFpBsLngUiEyAx');
+  // 	expect(socket.connected).to.equal(false);
+  // 	socket.on('connect', function(socket){
+  // 		console.log('connected');
+  // 		done();
+  // 	})
+  // });
+
 });
